88df45a0fba7195e37070681957e80cc14670949
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-11 12:45:39 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -15,7 +15,8 @@ def prompt_user
 end
 
 def get_user_input
-  answer = gets.chomp# code #get_user_input here
+  answer = gets.chomp
+  return answer# code #get_user_input here
 end
 
 def end_game(card_total)
